f86dc01794faabea957f0c25a76ff53cf496b72f
siprianotrader <150560820+siprianotrader@users.noreply.github.com>
2025-02-21 20:28:04 -0400

    Tropical Lanches 1.0.6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "FSW Donalds",
+  title: "Tropical | Lanches",
   description: "Bora finalizar esse projeto lindo!",
 };
 
